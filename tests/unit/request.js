8cc7c09c9b62da1d8a19dc3740bc7d70e4d9c752
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2016-08-21 14:33:54 +0200

    UT request

--- tests/unit/request.js
+++ tests/unit/request.js
@@ -8,5 +8,33 @@ const expect = chai.expect;
 
 describe("Request", () => {
 
+  const mockValue = "123456789";
+  const mockSourceChannel = {};
+  const mockTargetChannel = {};
 
+  const request = new Horpyna.Request(mockValue, mockSourceChannel, mockTargetChannel);
+
+  describe("getValue method", () => {
+
+    it("should return value", done => {
+      expect(mockValue).to.be.equal(request.getValue());
+      done();
+    });
+  });
+
+  describe("getTarget method", () => {
+
+    it("should return target channel", done => {
+      expect(mockTargetChannel).to.be.equal(request.getTarget());
+      done();
+    });
+  });
+
+  describe("getSource method", () => {
+
+    it("should return source channel", done => {
+      expect(mockSourceChannel).to.be.equal(request.getSource());
+      done();
+    });
+  });
 });
