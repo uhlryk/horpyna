b71f6c7dc5745125c3bc99fc719aabd01a51c36a
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2018-01-24 22:51:06 +0100

    update example

--- example/index.js
+++ example/index.js
@@ -1,3 +1,22 @@
-import Horpyna from "./../dist/";
+import Horpyna from "../dist/";
 
-const node = Horpyna.createNode();
+const mainBranch = new Horpyna.Branch({
+    name: "mainBranch",
+    branches: [
+        {
+            name: "calcBranch",
+            condition: value => value < 100000,
+            action: value => ++value
+        }
+    ]
+});
+mainBranch.getBranch("calcBranch").addBranch(mainBranch);
+console.time("measureIteration");
+mainBranch.execute(0).then(response => {
+    console.log("finished", response);
+    console.timeEnd("measureIteration"); //988.085ms
+});
+
+console.time("comparisonIteration");
+for (let i = 0; i < 100000; i++) {}
+console.timeEnd("comparisonIteration"); //0.843ms
